351c7950399e9339075e7d4105b4097691d8cb34
Dragos Daian <daian.dragos@yahoo.com>
2025-01-28 13:22:24 +0100

    fixes

--- godotsteam.h
+++ godotsteam.h
@@ -17,18 +17,17 @@
 #if defined(__GNUC__) && !defined(__clang__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
-#pragma GCC diagnostic ignored "-Werror=alloc-zero"
 
 #elif defined(__clang__)
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wnon-virtual-dtor"
-#pragma clang diagnostic ignored "-Werror=alloc-zero"
 #endif
 
 #if defined(_MSC_VER)
 // Errors from steam api
 // Warning: modules\godotsteam\godotsteam_enums.h(1555): warning C4309: 'initializing': truncation of constant value
 // Warning: modules\godotsteam\godotsteam_enums.h(1555): warning C4369: 'REMOTE_STORAGE_PLATFORM_ALL':  enumerator value '-1' cannot be represented as 'unsigned int', value is '-1'
+#pragma warning(push)
 #pragma warning(disable : 4309 4369)
 #endif
 // Include Steamworks API headers
@@ -40,6 +39,10 @@
 #pragma GCC diagnostic pop
 #elif defined(__clang__)
 #pragma clang diagnostic pop
+#endif
+
+#if defined(_MSC_VER)
+#pragma warning(pop)
 #endif
 
 // Include Godot headers
